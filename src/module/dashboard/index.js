c5e5f47f34d898253f3ca1aa03ed6b5ac7182d9b
santihera <santiherakusumawati@gmail.com>
2023-08-02 11:34:54 +0700

    mengubah halaman dashboard

--- src/module/dashboard/index.js
+++ src/module/dashboard/index.js
@@ -1,11 +1,11 @@
 import Layout from "@/components/Layout";
 
-export default function DashboardPage(){
+export default function DashboardPage() {
   return (
     <>
       <Layout title='Dashboard Page'>
         <main>
-          <p>Dashboard Page</p>
+          <p>Halaman Dashboard Page</p>
         </main>
       </Layout>
     </>
